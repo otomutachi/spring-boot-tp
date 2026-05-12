5da280937a486b425ffaa4b5a34a80df7ce31bf4
Maviet cedric <cedric.maviet@gmail.com>
2026-05-12 11:18:35 +1100

    ajout du nv libre

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,11 +2,13 @@ INSERT INTO auteurs (nom, prenom) VALUES ('Tolkien', 'J.R.R.');
 INSERT INTO auteurs (nom, prenom) VALUES ('Kishimoto', 'Masashi');
 INSERT INTO auteurs (nom, prenom) VALUES ('Baudelaire', 'Charles');
 INSERT INTO auteurs (nom, prenom) VALUES ('Goscinny', 'René');
+INSERT INTO auteurs (nom, prenom) VALUES ('Liebert', 'Johann');
 
 INSERT INTO livres (titre, isbn, categorie, auteur_id) VALUES ('Le Seigneur des Anneaux', '978-2-07-061190-3', 'ROMAN', 1);
 INSERT INTO livres (titre, isbn, categorie, auteur_id) VALUES ('Naruto Vol.1', '978-2-87-128280-2', 'MANGA', 2);
 INSERT INTO livres (titre, isbn, categorie, auteur_id) VALUES ('Les Fleurs du Mal', '978-2-07-036024-3', 'POESIE', 3);
 INSERT INTO livres (titre, isbn, categorie, auteur_id) VALUES ('Astérix le Gaulois', '978-2-86-497001-9', 'COMEDIE', 4);
+INSERT INTO livres (titre, isbn, categorie, auteur_id) VALUES ('Monster', '978-2-86-497002-6', 'ROMAN', 5);
 
 INSERT INTO utilisateur (nom, prenom, email, telephone) VALUES ('Dupont', 'Jean', 'jean.dupont@email.com', '0123456789');
 INSERT INTO utilisateur (nom, prenom, email, telephone) VALUES ('Martin', 'Marie', 'marie.martin@email.com', '0987654321');
